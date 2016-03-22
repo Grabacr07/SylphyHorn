4440ce255c072c1e6caf16fd79a7ffbddcbbfa89
Grabacr07 <grabacr07+github@gmail.com>
2016-03-22 19:13:48 +0900

    Update version.

--- source/SylphyHorn/Properties/AssemblyInfo.cs
+++ source/SylphyHorn/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
