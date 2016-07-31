1daae80ebe0cb13c29fec285474df27a17cb481d
Manato KAMEYA <grabacr07+github@gmail.com>
2016-08-01 03:23:17 +0900

    Add 'ApplicationPreparation" class.

--- source/SylphyHorn/Application.xaml.cs
+++ source/SylphyHorn/Application.xaml.cs
@@ -12,11 +12,6 @@ using StatefulModel;
 using SylphyHorn.Serialization;
 using SylphyHorn.Services;
 using VDMHelperCLR.Common;
-using WindowsDesktop;
-using SylphyHorn.Interop;
-using SylphyHorn.UI;
-using SylphyHorn.UI.Bindings;
-using MessageBox = System.Windows.MessageBox;
 
 namespace SylphyHorn
 {
@@ -49,7 +44,7 @@ namespace SylphyHorn
 			if (appInstance.IsFirst || Args.Restarted.HasValue)
 #endif
 			{
-				if (VirtualDesktop.IsSupported)
+				if (WindowsDesktop.VirtualDesktop.IsSupported)
 				{
 					this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
 					this.DispatcherUnhandledException += (sender, args) =>
@@ -65,13 +60,14 @@ namespace SylphyHorn
 
 					ThemeService.Current.Register(this, Theme.Windows, Accent.Windows);
 
-					this.ShowTaskTrayIcon();
-
 					this.VdmHelper = VdmHelperFactory.CreateInstance().AddTo(this);
 					this.VdmHelper.Init();
 					this.HookService = new HookService(this.VdmHelper).AddTo(this);
 					this.InteropService = new UwpInteropService(this.HookService, Settings.General).AddTo(this);
-					this.RegisterActions();
+
+					var preparation = new ApplicationPreparation(this);
+					preparation.ShowTaskTrayIcon();
+					preparation.RegisterActions();
 
 					NotificationService.Instance.AddTo(this);
 					WallpaperService.Instance.AddTo(this);
@@ -100,90 +96,6 @@ namespace SylphyHorn
 			((IDisposable)this).Dispose();
 		}
 
-
-		private void RegisterActions()
-		{
-			var settings = Settings.ShortcutKey;
-
-			this.HookService
-				.Register(settings.MoveLeft.ToShortcutKey(), hWnd => hWnd.MoveToLeft(this.VdmHelper))
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.MoveLeftAndSwitch.ToShortcutKey(), hWnd => hWnd.MoveToLeft(this.VdmHelper)?.Switch())
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.MoveRight.ToShortcutKey(), hWnd => hWnd.MoveToRight(this.VdmHelper))
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.MoveRightAndSwitch.ToShortcutKey(), hWnd => hWnd.MoveToRight(this.VdmHelper)?.Switch())
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.MoveNew.ToShortcutKey(), hWnd => hWnd.MoveToNew(this.VdmHelper))
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.MoveNewAndSwitch.ToShortcutKey(), hWnd => hWnd.MoveToNew(this.VdmHelper)?.Switch())
-				.AddTo(this);
-
-			this.HookService
-				.Register(
-					settings.SwitchToLeft.ToShortcutKey(),
-					_ => VirtualDesktopService.GetLeft()?.Switch(),
-					() => Settings.General.OverrideWindowsDefaultKeyCombination || Settings.General.ChangeBackgroundEachDesktop)
-				.AddTo(this);
-
-			this.HookService
-				.Register(
-					settings.SwitchToRight.ToShortcutKey(),
-					_ => VirtualDesktopService.GetRight()?.Switch(),
-					() => Settings.General.OverrideWindowsDefaultKeyCombination || Settings.General.ChangeBackgroundEachDesktop)
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.Pin.ToShortcutKey(), hWnd => hWnd.Pin())
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.Unpin.ToShortcutKey(), hWnd => hWnd.Unpin())
-				.AddTo(this);
-
-			this.HookService
-				.Register(settings.TogglePin.ToShortcutKey(), hWnd => hWnd.TogglePin())
-				.AddTo(this);
-		}
-
-		private void ShowTaskTrayIcon()
-		{
-			const string iconUri = "pack://application:,,,/SylphyHorn;Component/Assets/tasktray.ico";
-
-			Uri uri;
-			if (!Uri.TryCreate(iconUri, UriKind.Absolute, out uri)) return;
-
-			var icon = IconHelper.GetIconFromResource(uri);
-			var menus = new[]
-			{
-				new TaskTrayIconItem("&Settings (S)", () => this.ShowSettings(), () => Args.CanSettings),
-				new TaskTrayIconItem("E&xit (X)", () => this.Shutdown()),
-			};
-
-			var taskTrayIcon = new TaskTrayIcon(icon, menus);
-			taskTrayIcon.Show();
-			taskTrayIcon.AddTo(this);
-		}
-
-		private void ShowSettings()
-		{
-			using (this.HookService.Suspend())
-			{
-				var window = new SettingsWindow { DataContext = new SettingsWindowViewModel(this.HookService), };
-				window.ShowDialog();
-			}
-		}
-
 		private static void SetupShortcut()
 		{
 
@@ -203,9 +115,12 @@ args = {2}
 
 			#endregion
 
+			var message = "";
+
 			try
 			{
-				var message = string.Format(messageFormat,
+				message = string.Format(
+					messageFormat,
 					DateTimeOffset.Now,
 					sender,
 					Environment.GetCommandLineArgs().Skip(1).JoinString(" "),
@@ -213,32 +128,38 @@ args = {2}
 
 				Debug.WriteLine(message);
 				File.AppendAllText(path, message);
-				MessageBox.Show(message, "Unexpected error occurred");
 			}
 			catch (Exception ex)
 			{
 				Debug.WriteLine(ex);
 			}
 
-			if (Args != null)
+			try
 			{
-
-				var restartNum = Args.Restarted ?? 0;
-				if ((DateTime.Now - Process.GetCurrentProcess().StartTime).TotalMinutes > 3)
+				if (Args != null)
 				{
-					// 3 分以上生きてたら安定稼働と見做して、とりあえず再起動させる
-					Process.Start(
-						Environment.GetCommandLineArgs()[0],
-						Args.Options
-							.Where(x => x.Key != Args.GetKey(nameof(CommandLineArgs.Restarted)))
-							.Concat(EnumerableEx.Return(Args.CreateOption(nameof(CommandLineArgs.Restarted), (restartNum + 1).ToString())))
-							.Select(x => x.ToString())
-							.JoinString(" "));
+					var restartNum = Args.Restarted ?? 0;
+					if ((DateTime.Now - Process.GetCurrentProcess().StartTime).TotalMinutes >= 3)
+					{
+						// 3 分以上生きてたら安定稼働と見做して、とりあえず再起動させる
+						Process.Start(
+							Environment.GetCommandLineArgs()[0],
+							Args.Options
+								.Where(x => x.Key != Args.GetKey(nameof(CommandLineArgs.Restarted)))
+								.Concat(EnumerableEx.Return(Args.CreateOption(nameof(CommandLineArgs.Restarted), (restartNum + 1).ToString())))
+								.Select(x => x.ToString())
+								.JoinString(" "));
+					}
+					else
+					{
+						// ToDo: 例外ダイアログ
+						MessageBox.Show(message, "Unexpected error occurred");
+					}
 				}
-				else
-				{
-					// ToDo: 例外ダイアログ
-				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
 			}
 
 			Current.Shutdown();
---
+++ source/SylphyHorn/ApplicationPreparation.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MetroTrilithon.Lifetime;
+using SylphyHorn.Interop;
+using SylphyHorn.Serialization;
+using SylphyHorn.Services;
+using SylphyHorn.UI;
+using SylphyHorn.UI.Bindings;
+
+namespace SylphyHorn
+{
+	public class ApplicationPreparation
+	{
+		private readonly Application _application;
+
+		public ApplicationPreparation(Application application)
+		{
+			this._application = application;
+		}
+
+		public void RegisterActions()
+		{
+			var settings = Settings.ShortcutKey;
+
+			this._application.HookService
+				.Register(settings.MoveLeft.ToShortcutKey(), hWnd => hWnd.MoveToLeft(this._application.VdmHelper))
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.MoveLeftAndSwitch.ToShortcutKey(), hWnd => hWnd.MoveToLeft(this._application.VdmHelper)?.Switch())
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.MoveRight.ToShortcutKey(), hWnd => hWnd.MoveToRight(this._application.VdmHelper))
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.MoveRightAndSwitch.ToShortcutKey(), hWnd => hWnd.MoveToRight(this._application.VdmHelper)?.Switch())
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.MoveNew.ToShortcutKey(), hWnd => hWnd.MoveToNew(this._application.VdmHelper))
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.MoveNewAndSwitch.ToShortcutKey(), hWnd => hWnd.MoveToNew(this._application.VdmHelper)?.Switch())
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(
+					settings.SwitchToLeft.ToShortcutKey(),
+					_ => VirtualDesktopService.GetLeft()?.Switch(),
+					() => Settings.General.OverrideWindowsDefaultKeyCombination || Settings.General.ChangeBackgroundEachDesktop)
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(
+					settings.SwitchToRight.ToShortcutKey(),
+					_ => VirtualDesktopService.GetRight()?.Switch(),
+					() => Settings.General.OverrideWindowsDefaultKeyCombination || Settings.General.ChangeBackgroundEachDesktop)
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.Pin.ToShortcutKey(), hWnd => hWnd.Pin())
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.Unpin.ToShortcutKey(), hWnd => hWnd.Unpin())
+				.AddTo(this._application);
+
+			this._application.HookService
+				.Register(settings.TogglePin.ToShortcutKey(), hWnd => hWnd.TogglePin())
+				.AddTo(this._application);
+		}
+
+
+		public void ShowTaskTrayIcon()
+		{
+			const string iconUri = "pack://application:,,,/SylphyHorn;Component/Assets/tasktray.ico";
+
+			Uri uri;
+			if (!Uri.TryCreate(iconUri, UriKind.Absolute, out uri)) return;
+
+			var icon = IconHelper.GetIconFromResource(uri);
+			var menus = new[]
+			{
+				new TaskTrayIconItem("&Settings (S)", () => this.ShowSettings(), () => Application.Args.CanSettings),
+				new TaskTrayIconItem("E&xit (X)", () => this._application.Shutdown()),
+			};
+
+			var taskTrayIcon = new TaskTrayIcon(icon, menus);
+			taskTrayIcon.Show();
+			taskTrayIcon.AddTo(this._application);
+		}
+
+		private void ShowSettings()
+		{
+			using (this._application.HookService.Suspend())
+			{
+				var window = new SettingsWindow
+				{
+					DataContext = new SettingsWindowViewModel(this._application.HookService),
+				};
+				window.ShowDialog();
+			}
+		}
+	}
+}
