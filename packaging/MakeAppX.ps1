e26085974aa84acf840a435a1d81731e9e27c1bf
Grabacr07 <grabacr07+github@gmail.com>
2016-12-12 03:25:29 +0900

    Add appxbundle creation function to MakeAppx.ps1

--- packaging/MakeAppX.ps1
+++ packaging/MakeAppX.ps1
@@ -8,13 +8,17 @@
         $targetKeywords = '*.exe','*.dll','*.exe.config','*.txt','*.VisualElementsManifest.xml', '*.png', 'AppxManifest.xml'
         $ignoreKeyword  = '*.vshost.*'
 
-        $package = 'SylphyHorn.appx'
+        $appxPackage = 'SylphyHorn.appx'
+        $appxBundle = 'SylphyHorn.appxbundle'
+        $storekey = 'SylphyHorn.StoreKey.pfx'
         $publisher = 'CN=33C1D2CA-4B3F-4CCA-8103-6D02939C6477'
         
-        $mapping = '_SylphyHorn.map'
-        $tempCer = '_SylphyHorn.cer'
-        $tempPvk = '_SylphyHorn.pvk'
-        $tempPfx = '_SylphyHorn.pfx'
+        $appxMapping = '_SylphyHorn.appx.map'
+        $appxBundleMapping = '_SylphyHorn.appxbundle.map'
+
+        #$tempCer = '_SylphyHorn.cer'
+        #$tempPvk = '_SylphyHorn.pvk'
+        #$tempPfx = '_SylphyHorn.pfx'
 
         $win10Sdk = 'C:\Program Files (x86)\Windows Kits\10\bin\x64' 
  
@@ -33,14 +37,20 @@
             Get-ChildItem | where { $_.Extension -eq ".appx" } | Remove-Item
             Get-ChildItem | where Name -Like "_*" | Remove-Item
  
-            New-MappingFile -SourcePath $(Join-Path $bin $target) -Filename $mapping -Targets $targetKeywords -Exclude $ignoreKeyword
+            New-AppxMappingFile -SourcePath $(Join-Path $bin $target) -Filename $appxMapping -Targets $targetKeywords -Exclude $ignoreKeyword
 
-            Start-SdkTool -SdkPath $win10Sdk -ToolName 'makeappx' -Arguments "pack /l /f $mapping /p $package"
-            Start-SdkTool -SdkPath $win10Sdk -ToolName 'makecert' -Arguments "/r /h 0 /n $publisher /eku `"1.3.6.1.5.5.7.3.3`" /pe /sv $tempPvk $tempCer"
-            Start-SdkTool -SdkPath $win10Sdk -ToolName 'pvk2pfx'  -Arguments "/pvk $tempPvk /spc $tempCer /pfx $tempPfx"
-            Start-SdkTool -SdkPath $win10Sdk -ToolName 'signtool' -Arguments "sign /f $tempPfx /fd SHA256 /v $package"
+            Start-SdkTool -SdkPath $win10Sdk -ToolName 'makeappx' -Arguments "pack /l /f $appxMapping /p $appxPackage"
+            Start-SdkTool -SdkPath $win10Sdk -ToolName 'signtool' -Arguments "sign /f $storekey /fd SHA256 /v $appxPackage"            
+            
+            # if you don't have pfx file, can create that by following commands.
+            #Start-SdkTool -SdkPath $win10Sdk -ToolName 'makecert' -Arguments "/r /h 0 /n $publisher /eku `"1.3.6.1.5.5.7.3.3`" /pe /sv $tempPvk $tempCer"
+            #Start-SdkTool -SdkPath $win10Sdk -ToolName 'pvk2pfx'  -Arguments "/pvk $tempPvk /spc $tempCer /pfx $tempPfx"
+            #Start-SdkTool -SdkPath $win10Sdk -ToolName 'signtool' -Arguments "sign /f $tempPfx /fd SHA256 /v $package"
+            #Certutil -addStore TrustedPeople $tempCer
 
-            Certutil -addStore TrustedPeople $tempCer
+            New-AppxBundleMappingFile -SourcePath '.\' -Filename $appxBundleMapping -Targets $appxPackage
+
+            Start-SdkTool -SdkPath $win10Sdk -ToolName 'makeappx' -Arguments "bundle /f $appxBundleMapping /p $appxBundle"
         }
         catch
         {
@@ -49,7 +59,7 @@
     }
 }
 
-function New-MappingFile
+function New-AppxMappingFile
 {
     [CmdletBinding()]
     param
@@ -95,6 +105,57 @@ function New-MappingFile
         Foreach ($target in $Targets)
         {
             $files += Get-ChildItem -Path $SourcePath -Recurse -Filter $target | where Name -NotLike $Exclude
+        }
+
+        Foreach ($file in $files | sort)
+        {
+            $mapping += "`"{0}`" `"{1}`"`n" -f $file.FullName, $file.FullName.Replace((Convert-Path $SourcePath) + "\", "")
+        }        
+
+        Write-Output $mapping
+        Write-Output $mapping | Out-File $Filename
+    }
+}
+
+function New-AppxBundleMappingFile
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(
+            mandatory = 1,
+            position  = 0,
+            ValueFromPipeline = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [string]
+        $SourcePath,
+ 
+        [parameter(
+            mandatory = 1,
+            position  = 1,
+            ValueFromPipelineByPropertyName = 1)]
+        [string]
+        $Filename,
+ 
+        [parameter(
+            mandatory = 1,
+            position  = 2,
+            ValueFromPipelineByPropertyName = 1)]
+        [string[]]
+        $Targets
+    )
+ 
+    begin
+    {
+        $files = New-Object 'System.Collections.Generic.List[String]'
+        $mapping = "[Files]`n"
+    }
+ 
+    end
+    {
+        Foreach ($target in $Targets)
+        {
+            $files += Get-ChildItem -Path $SourcePath -Recurse -Filter $target
         }
 
         Foreach ($file in $files | sort)
