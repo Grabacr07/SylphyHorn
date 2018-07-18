077ca3917801a3e2d8ff46f57ae43bf38a3291d3
Grabacr07 <grabacr07+github@gmail.com>
2018-07-18 23:34:09 +0900

    Fixed an issue running on Desktop Bridge.

--- source/SylphyHorn/Properties/Directories.cs
+++ source/SylphyHorn/Properties/Directories.cs
@@ -5,7 +5,16 @@ namespace SylphyHorn.Properties
 {
 	public static class Directories
 	{
-		private static readonly Lazy<DirectoryInfo> _localAppDataLazy = new Lazy<DirectoryInfo>(() => new DirectoryInfo(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), ProductInfo.Company, ProductInfo.Product)));
+		private static readonly Lazy<DirectoryInfo> _localAppDataLazy = new Lazy<DirectoryInfo>(
+			() => new DirectoryInfo(
+				Path.Combine(
+					Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+#if APPX
+					@"Packages\46846grabacr.net.SylphyHorn_vwznf8jfphrrc\LocalCache\Local",
+#endif
+					ProductInfo.Company,
+					ProductInfo.Product)
+			));
 
 		internal static DirectoryInfo LocalAppData => _localAppDataLazy.Value;
 	}
