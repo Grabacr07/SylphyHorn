0c890acd7cd135d57e237a14364693aba82e9a1a
Victor <virb3e@gmail.com>
2020-01-27 16:03:34 +0000

    Remove UWP support

--- source/SylphyHorn/Startup.cs
+++ source/SylphyHorn/Startup.cs
@@ -4,18 +4,14 @@ using System.Reflection;
 using MetroTrilithon.Desktop;
 using MetroTrilithon.Threading.Tasks;
 using SylphyHorn.Interop;
-using Windows.ApplicationModel;
 
 namespace SylphyHorn
 {
 	public class Startup
 	{
 		private readonly string _path;
-		private readonly StartupTask _task;
 
-		public bool IsExists => Platform.IsUwp
-			? this._task.State == StartupTaskState.Enabled || this._task.State == StartupTaskState.DisabledByUser
-			: File.Exists(this._path);
+		public bool IsExists => File.Exists(this._path);
 
 		public Startup()
 			: this(GetExecutingAssemblyFileNameWithoutExtension())
@@ -24,46 +20,19 @@ namespace SylphyHorn
 
 		public Startup(string name)
 		{
-			if (Platform.IsUwp)
-			{
-				this._task = GetStartupTask();
-			}
-			else
-			{
-				this._path = GetStartupFilePath(name);
-			}
+			this._path = GetStartupFilePath(name);
 		}
 
 		public void Create()
 		{
-			if (Platform.IsUwp)
-			{
-				if (this._task.State == StartupTaskState.Disabled)
-				{
-					this._task.RequestEnableAsync().ToTask().Forget();
-				}
-			}
-			else
-			{
-				ShellLink.Create(this._path);
-			}
+			ShellLink.Create(this._path);
 		}
 
 		public void Remove()
 		{
-			if (Platform.IsUwp)
+			if (this.IsExists)
 			{
-				if (this._task.State == StartupTaskState.Enabled || this._task.State == StartupTaskState.DisabledByUser)
-				{
-					this._task.Disable();
-				}
-			}
-			else
-			{
-				if (this.IsExists)
-				{
-					File.Delete(this._path);
-				}
+				File.Delete(this._path);
 			}
 		}
 
@@ -76,11 +45,6 @@ namespace SylphyHorn
 		private static string GetExecutingAssemblyFileNameWithoutExtension()
 		{
 			return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location);
-		}
-
-		private static StartupTask GetStartupTask()
-		{
-			return StartupTask.GetAsync("SylphyHornEngineStartupTask").ToTask().Result;
 		}
 	}
 }
